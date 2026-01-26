16f51b362e5f8f2d9afc26ca0d3e92eaa357dcac
Elean1402 <eric.lean.maier93@gmail.com>
2026-01-26 14:34:48 +0100

    Converted Player/Npc into Object struct

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,40 @@ const LIMIT_FPS: i32 = 20; //20 frames per sec maximum
 
 struct Tcod {
     root: Root,
+    con: Offscreen,
 }
 
-struct Player {
+struct Object {
     x: i32,
     y: i32,
+    char: char,
+    color: Color,
 }
 
-fn handle_keys(tcod: &mut Tcod, player: &mut Player) -> bool {
+impl Object {
+    pub fn new(x: i32, y: i32, char: char, color: Color) -> Self {
+        Object { x, y, char, color }
+    }
+
+    // move by the given amount
+    pub fn move_by(&mut self, dx: i32, dy: i32) {
+        self.x += dx;
+        self.y += dy;
+    }
+
+    // set the color and then draw the character that represents this object at its position
+    pub fn draw(&self, con: &mut dyn Console) {
+        con.set_default_foreground(self.color);
+        con.put_char(self.x, self.y, self.char, BackgroundFlag::None);
+    }
+}
+
+struct Objects {
+    player: Object,
+    npc: Object,
+}
+
+fn handle_keys(tcod: &mut Tcod, player: &mut Object) -> bool {
     use tcod::input::Key;
     use tcod::input::KeyCode::*;
 
@@ -32,11 +58,10 @@ fn handle_keys(tcod: &mut Tcod, player: &mut Player) -> bool {
         }
         Key { code: Escape, .. } => return true, // exit game
         // movement keys
-        Key { code: Up, .. } => player.y -= 1,
-        Key { code: Down, .. } => player.y += 1,
-        Key { code: Left, .. } => player.x -= 1,
-        Key { code: Right, .. } => player.x += 1,
-
+        Key { code: Up, .. } => player.move_by(0, -1),
+        Key { code: Down, .. } => player.move_by(0, 1),
+        Key { code: Left, .. } => player.move_by(-1, 0),
+        Key { code: Right, .. } => player.move_by(1, 0),
         _ => {}
     }
     false
@@ -49,23 +74,35 @@ fn main() {
             .font_type(FontType::Greyscale)
             .size(SCREEN_WIDTH, SCREEN_HEIGHT)
             .title("Rust/libtcod tutorial")
-            .init() };
+            .init(),
+        con:Offscreen::new(SCREEN_WIDTH, SCREEN_HEIGHT)
+    };
 
     tcod::system::set_fps(LIMIT_FPS);
 
-    let mut player = Player {
-        x: SCREEN_WIDTH / 2,
-        y: SCREEN_HEIGHT / 2,
-    };
+    let player = Object::new(SCREEN_WIDTH / 2,SCREEN_HEIGHT / 2,'@', WHITE);
+    let npc = Object::new(SCREEN_WIDTH / 2 - 5, SCREEN_HEIGHT / 2, '@', YELLOW);
+    let mut objects = [player, npc];
 
     while !tcod.root.window_closed() {
-        tcod.root.set_default_foreground(WHITE);
-        tcod.root.clear();
-        tcod.root.put_char(player.x, player.y, '@', BackgroundFlag::None);
+        // clear the screen of the previous frame
+        tcod.con.clear();
+        for object in &objects {
+            object.draw(&mut tcod.con);
+        }
+        blit(
+            &tcod.con,
+            (0, 0),
+            (SCREEN_WIDTH, SCREEN_HEIGHT),
+            &mut tcod.root,
+            (0, 0),
+            1.0,
+            1.0,
+        );
         tcod.root.flush();
         tcod.root.wait_for_keypress(true);
         // handle keys and exit game if needed
-        let exit = handle_keys(&mut tcod, &mut player);
+        let exit = handle_keys(&mut tcod, &mut objects[0]);
         if exit {
             break;
         }
