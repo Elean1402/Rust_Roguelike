5a786577c2fef69c0490715ecab50f134f26334e
Elean1402 <eric.lean.maier93@gmail.com>
2026-01-26 17:12:46 +0100

    Completed Part 2: The object and the map
    > Co-authored-by: CharlinZentgraf <charlin.zentgraf@web.de>
    
    Signed-off-by: Elean1402 <eric.lean.maier93@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+use tcod::colors;
 use tcod::colors::*;
 use tcod::console::*;
 
@@ -5,24 +7,98 @@ use tcod::console::*;
 const SCREEN_WIDTH: i32 = 80;
 const SCREEN_HEIGHT: i32 = 50;
 const LIMIT_FPS: i32 = 20; //20 frames per sec maximum
+const MAP_WIDTH: i32 = 80;
+const MAP_HEIGHT: i32 = 45;
+const COLOR_DARK_WALL: Color = Color { r: 0, g: 0, b: 100 };
+const COLOR_DARK_GROUND: Color = Color {
+    r: 50,
+    g: 50,
+    b: 150,
+};
 
 struct Tcod {
     root: Root,
     con: Offscreen,
 }
 
+#[derive(Clone, Copy, Debug)]
+struct Tile {
+    blocked: bool,
+    block_sight: bool,
+}
+
+impl Tile {
+    pub fn empty() -> Self {
+        Tile {
+            blocked: false,
+            block_sight: false,
+        }
+    }
+    pub fn wall() -> Self {
+        Tile {
+            blocked: true,
+            block_sight: true,
+        }
+    }
+}
+
+type Map = Vec<Vec<Tile>>;
+
+struct Game {
+    map: Map,
+}
+
+fn make_map() -> Map {
+    // fill map with "unblocked" tiles
+    let mut map = vec![vec![Tile::empty(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
+
+    // place two pillars to test the map
+    map[30][22] = Tile::wall();
+    map[50][22] = Tile::wall();
+
+    map
+}
+
+fn render_all(tcod: &mut Tcod, game: &Game, objects: &Objects) {
+    // draw all objects in the list
+    objects.draw_all(&mut tcod.con);
+
+    // go through all tiles, and set their background color
+    for y in 0..MAP_HEIGHT {
+        for x in 0..MAP_WIDTH {
+            let wall = game.map[x as usize][y as usize].block_sight;
+            if wall {
+                tcod.con
+                    .set_char_background(x, y, COLOR_DARK_WALL, BackgroundFlag::Set);
+            } else {
+                tcod.con
+                    .set_char_background(x, y, COLOR_DARK_GROUND, BackgroundFlag::Set);
+            }
+        }
+    }
+
+    blit(
+        &tcod.con,
+        (0, 0),
+        (SCREEN_WIDTH, SCREEN_HEIGHT),
+        &mut tcod.root,
+        (0, 0),
+        1.0,
+        1.0,
+    );
+}
+
+#[derive(Debug)]
 struct Object {
     x: i32,
     y: i32,
     char: char,
     color: Color,
 }
-
 impl Object {
     pub fn new(x: i32, y: i32, char: char, color: Color) -> Self {
         Object { x, y, char, color }
     }
-
     // move by the given amount
     pub fn move_by(&mut self, dx: i32, dy: i32, game: &Game) {
         if !game.map[(self.x + dx) as usize][(self.y + dy) as usize].blocked {
@@ -30,7 +106,6 @@ impl Object {
             self.y += dy;
         }
     }
-
     // set the color and then draw the character that represents this object at its position
     pub fn draw(&self, con: &mut dyn Console) {
         con.set_default_foreground(self.color);
@@ -40,7 +115,18 @@ impl Object {
 
 struct Objects {
     player: Object,
-    npc: Object,
+    npcs: HashMap<String, Object>,
+}
+impl Objects {
+    pub fn new(player: Object, npcs: HashMap<String, Object>) -> Self {
+        Objects { player, npcs }
+    }
+    pub fn draw_all(&self, con: &mut dyn Console) {
+        for npc in self.npcs.values() {
+            npc.draw(con);
+        }
+        self.player.draw(con);
+    }
 }
 
 fn handle_keys(tcod: &mut Tcod, game: &Game, player: &mut Object) -> bool {
@@ -71,40 +157,42 @@ fn handle_keys(tcod: &mut Tcod, game: &Game, player: &mut Object) -> bool {
 
 fn main() {
     let mut tcod = Tcod {
-        root:Root::initializer()
+        root: Root::initializer()
             .font("arial10x10.png", FontLayout::Tcod)
             .font_type(FontType::Greyscale)
             .size(SCREEN_WIDTH, SCREEN_HEIGHT)
             .title("Rust/libtcod tutorial")
             .init(),
-        con:Offscreen::new(SCREEN_WIDTH, SCREEN_HEIGHT)
+        con: Offscreen::new(MAP_WIDTH, MAP_HEIGHT),
     };
 
     tcod::system::set_fps(LIMIT_FPS);
 
-    let player = Object::new(SCREEN_WIDTH / 2,SCREEN_HEIGHT / 2,'@', WHITE);
-    let npc = Object::new(SCREEN_WIDTH / 2 - 5, SCREEN_HEIGHT / 2, '@', YELLOW);
-    let mut objects = [player, npc];
+    let mut npcs = HashMap::new();
+    npcs.insert(
+        "bob".to_string(),
+        Object::new(SCREEN_WIDTH / 2 - 5, SCREEN_HEIGHT / 2, '@', YELLOW),
+    );
+    let mut objects = Objects::new(
+        Object::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, '@', WHITE),
+        npcs,
+    );
+    objects.player.draw(&mut tcod.con);
+
+    let game = Game {
+        // generate map (at this point it's not drawn to the screen)
+        map: make_map(),
+    };
 
     while !tcod.root.window_closed() {
         // clear the screen of the previous frame
         tcod.con.clear();
-        for object in &objects {
-            object.draw(&mut tcod.con);
-        }
-        blit(
-            &tcod.con,
-            (0, 0),
-            (SCREEN_WIDTH, SCREEN_HEIGHT),
-            &mut tcod.root,
-            (0, 0),
-            1.0,
-            1.0,
-        );
+        render_all(&mut tcod, &game, &objects);
         tcod.root.flush();
+
         tcod.root.wait_for_keypress(true);
         // handle keys and exit game if needed
-        let exit = handle_keys(&mut tcod, &mut objects[0]);
+        let exit = handle_keys(&mut tcod, &game, &mut objects.player);
         if exit {
             break;
         }
